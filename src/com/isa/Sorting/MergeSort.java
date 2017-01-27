7f9edb7f0344f962b529b0225f629312d71b364b
isa <isaliangyiyun@hotmail.com>
2017-01-26 23:12:32 -0500

    update merge sort

--- src/com/isa/Sorting/MergeSort.java
+++ src/com/isa/Sorting/MergeSort.java
@@ -1,7 +1,88 @@
 package com.isa.Sorting;
 
+import java.util.Arrays;
+
 /**
  * Created by isa on 2017-01-25.
  */
 public class MergeSort {
+     /*
+     * Time complexity: O(nlogn)
+     * Space complexity: O(n)
+     *
+     */
+
+    public static void mergeSort(int[] arr, int lower, int upper){
+        if(lower < upper) {
+            int mid = lower + (upper - lower)/2;
+
+            mergeSort(arr, lower, mid);
+            mergeSort(arr, mid+1, upper);
+            merge(arr, lower, mid, upper);
+        }
+    }
+
+    public static void merge(int[] arr, int lower, int mid, int upper){
+        int[] left = new int[mid-lower+1];
+        int[] right = new int[upper-mid+1];
+        int lenL = left.length;
+        int lenR = right.length;
+
+        int l = 0;
+        int r = 0;
+        int m = 0;
+        while(l < lenL && r < lenR){
+            if(left[l] <= right[r]){
+                arr[m] = left[l];
+                l++;
+            }else{
+                arr[m] = right[r];
+                r++;
+            }
+            m++;
+        }
+
+        while(l < lenL){
+            arr[m] = left[l];
+            l++;
+            m++;
+        }
+
+        while(r < lenR){
+            arr[m] = right[r];
+            r++;
+            m++;
+        }
+    }
+
+    public static void merge(int[] left, int[] right, int[] arr){
+        int lenL = left.length;
+        int lenR = right.length;
+
+        int l = 0;
+        int r = 0;
+        int m = 0;
+        while(l < lenL && r < lenR){
+            if(left[l] <= right[r]){
+                arr[m] = left[l];
+                l++;
+            }else{
+                arr[m] = right[r];
+                r++;
+            }
+            m++;
+        }
+
+        while(l < lenL){
+            arr[m] = left[l];
+            l++;
+            m++;
+        }
+
+        while(r < lenR){
+            arr[m] = right[r];
+            r++;
+            m++;
+        }
+    }
 }
--- src/com/isa/Sorting/SortingTest.java
+++ src/com/isa/Sorting/SortingTest.java
@@ -1,5 +1,7 @@
 package com.isa.Sorting;
 
+import com.sun.scenario.effect.Merge;
+
 /**
  * Created by isa on 2017-01-23.
  */
@@ -8,7 +10,8 @@ public class SortingTest {
     public static void main(String[] args) {
         int[] unsortedArr = {14, 15, 3, 7, 21, 5};
 
-        InsertionSort.insertionSort(unsortedArr);
+        //InsertionSort.insertionSort(unsortedArr);
+        MergeSort.mergeSort(unsortedArr, 0, unsortedArr.length-1);
 
         for(int i: unsortedArr){
             System.out.println(i);
