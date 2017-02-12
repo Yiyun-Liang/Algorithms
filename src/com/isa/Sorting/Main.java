8d253045f0b2eac94279be05220fce9769f5361b
isa <isaliangyiyun@hotmail.com>
2017-02-11 20:30:50 -0500

    add shell sort

--- src/com/isa/Sorting/Main.java
+++ src/com/isa/Sorting/Main.java
@@ -1,6 +1,7 @@
 package com.isa.Sorting;
 
 import com.isa.Sorting.quadratic.SelectionSort;
+import jdk.nashorn.tools.Shell;
 
 /**
  * Created by isa on 2017-01-23.
@@ -13,7 +14,8 @@ public class Main {
 
         //InsertionSort.insertionSort(unsortedArr);
         //MergeSort.mergeSort(unsortedArr, 0, 5);
-        SelectionSort.sort(unsortedArr);
+        //SelectionSort.sort(unsortedArr);
+        ShellSort.sort(unsortedArr);
 
         for(int i: unsortedArr){
             System.out.println(i);
--- src/com/isa/Sorting/ShellSort.java
+++ src/com/isa/Sorting/ShellSort.java
@@ -12,7 +12,44 @@ public class ShellSort {
         This is one of the oldest sorting method implemented by Shell in 1959
      */
 
-    public static void sort(int[] arr){
+    /*
+         Why insertion sort, eg. increments 7,3,1
+            - for bigger increments, smaller subarray
+            - for smaller increments, already partially sorted
 
+         Why shell sort is efficient
+            Proposition: a g-sorted array is still g-sorted after h-sorting it
+
+         Which increments to use?
+            - powers of 2? NO - did not sort odd elements until 1
+            - 3x+1? OK, easy to compute
+     */
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+
+        int h = 1;
+        while(h < n/3){
+            h = 3*h + 1;
+        }
+
+        while(h >= 1){
+            for(int i = h; i < n; i++){
+                for(int j = i; j >= h && less(arr[j], arr[j-h]); j -= h){
+                    swap(arr, j, j-h);
+                }
+            }
+            h = h/3;
+        }
+    }
+
+    public static boolean less(int a, int b){
+        return a < b;
+    }
+
+    public static void swap(int[] arr, int i1, int i2){
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 }
