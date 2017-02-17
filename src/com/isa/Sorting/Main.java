609627feb1b6d506120e017035c333437b6ff73c
isa <isaliangyiyun@hotmail.com>
2017-02-16 19:08:52 -0500

    add counting sort

--- src/com/isa/Sorting/Main.java
+++ src/com/isa/Sorting/Main.java
@@ -1,5 +1,6 @@
 package com.isa.Sorting;
 
+import com.isa.Sorting.n.CountingSort;
 import com.isa.Sorting.nlgn.HeapSort;
 import com.isa.Sorting.nlgn2.ShellSort;
 import com.isa.Sorting.quadraticOrNlgn.QuickSort;
@@ -13,13 +14,15 @@ public class Main {
         // 3,5,7,12,15,21
         int[] unsortedArr = {14, 15, 3, 7, 21, 5};
         int[] unsortedArrForHeapSort = {0, 14, 15, 3, 7, 21, 5};
+        int[] unsortedArrForCountingSort = {7, 5, 5, 3, 2, 7, 0, 6};
 
         //InsertionSort.insertionSort(unsortedArr);
         //MergeSort.mergeSort(unsortedArr, 0, 5);
         //SelectionSort.sort(unsortedArr);
         //ShellSort.sort(unsortedArr);
         //HeapSort.sort(unsortedArrForHeapSort);
-        QuickSort.sort(unsortedArr);
+        //QuickSort.sort(unsortedArr);
+        CountingSort.sort(unsortedArrForCountingSort, 7);
 
         for(int i: unsortedArr){
             System.out.println(i);
--- src/com/isa/Sorting/n/CountingSort.java
+++ src/com/isa/Sorting/n/CountingSort.java
@@ -4,4 +4,33 @@ package com.isa.Sorting.n;
  * Created by isa on 2017-02-15.
  */
 public class CountingSort {
+
+    // numbers need to be in range [0, range]
+    public static void sort(int[] arr, int range){
+        int[] brr = new int[arr.length];
+        sort(arr, brr, range);
+        arr = brr;
+    }
+
+    // O(k+n)
+    public static void sort(int[] arr, int[] brr, int k){
+        // initialize to zeros
+        int[] crr = new int[k+1];
+
+        // contains the number of elements equal to i
+        for(int j = 0; j < arr.length; j++){
+            crr[arr[j]] += 1;
+        }
+
+        // contains the number of elements less than or equal to i
+        for(int i = 1; i <= k; i++){
+            crr[i] = crr[i] + crr[i-1];
+        }
+
+        // move to right position in final array brr, is stable
+        for(int j = arr.length-1; j >= 0; j--){
+            brr[crr[arr[j]]] = arr[j];
+            crr[arr[j]] -= 1;
+        }
+    }
 }
