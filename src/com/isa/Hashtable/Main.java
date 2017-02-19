49bc705b6fb554cbb7e31ad70bf35a10f4d706f2
isa <isaliangyiyun@hotmail.com>
2017-02-19 15:23:09 -0500

    add performance analysis

--- src/com/isa/Hashtable/Main.java
+++ src/com/isa/Hashtable/Main.java
@@ -18,15 +18,19 @@ public class Main {
                                         in the hashtable, independently of where any other key has hashed to.
 
         Collisions:
+
+        Performance: 1 and 2 both have average time of O(1) for basic dictionary operations
+
             1) chaining: better when keys must be deleted
                 - doubly linked list if want to support quick deletion
+                - simplest resolution
 
             2) open addressing: avoids pointers to occupy extra space, at most one element/slot
 
                  to compute probe sequences:
                      - one approach is to apply uniform hashing(too ideal): can produce any probe sequence (1/m!)
                         - this is hard to implement
-                        
+
                  these all guarantees that <h(k,0),h(k,1)...h(k,m-1)> is a permutation of <0,1,...,m-1>
                  none of them satisfies uniform hashing, can generate at most m^2 sequences(instead of m!)
                      - linear probing: (h(k)+i)mod m starting from the initial slot
@@ -42,5 +46,11 @@ public class Main {
                         - has the greatest number of probe sequences(m^2), seems to give the best result
                         - must make sure all slots can be visited
                         - closest approach to uniform hashing
+
+                 Performance:
+                    hash table   |  number of probes in a successful search
+                    -------------------------------------------------------
+                     half full   |            < 1.387
+                     90 % full   |            < 2.559
      */
 }
