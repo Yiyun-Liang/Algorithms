22e0cd5b25d60cfbdfbe3809fa9ff36d890751ab
isa <isaliangyiyun@hotmail.com>
2017-02-27 19:06:12 -0500

    start dp

---
+++ src/com/isa/DynamicProgramming/Main.java
@@ -0,0 +1,7 @@
+package com.isa.DynamicProgramming;
+
+/**
+ * Created by isa on 2017-02-26.
+ */
+public class Main {
+}
---
+++ src/com/isa/DynamicProgramming/RodCutting.java
@@ -0,0 +1,32 @@
+package com.isa.DynamicProgramming;
+
+/**
+ * Created by isa on 2017-02-27.
+ */
+public class RodCutting {
+    /*
+        split problems into subproblems and memoize the solution
+
+        Use to-down memoization or bottom-up dp technique to get polynomial time solution rather than exponential
+     */
+
+    // O(2^n) time
+    public void rodCuttingNaive(int price[]){
+
+    }
+
+    // O(n^2) time
+    // add memoization table on top of the naive solution
+    public void rodCuttingTopDown(int price[]){
+
+    }
+
+    // O(n^2) time
+    // solve subproblems in order from smallest to biggest, so that
+    // when the bigger problems come, all the sub smaller problems that this solution for bigger problem needed
+    // is already solved and saved in a table
+    public void rodCuttingBottomUp(int price[]){
+
+    }
+
+}
