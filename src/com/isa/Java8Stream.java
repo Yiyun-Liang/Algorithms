22a14ae90982dc53776756772ec5791390c34550
isa <isaliangyiyun@hotmail.com>
2017-03-19 12:45:03 -0400

    add practice for stream flatmap

--- src/com/isa/Java8Stream.java
+++ src/com/isa/Java8Stream.java
@@ -296,6 +296,67 @@ public class Java8Stream {
                 .collect(personNameCollector);
 
         System.out.println(names);  // MAX | PETER | PAMELA | DAVID
+
+
+        // FLATMAP
+        // map: transform the objects of a stream into another type of objects by utilizing
+        // Map is kinda limited because every object can only be mapped to exactly one other object.
+        // flatmap: if we want to transform one object into multiple others or none at all.
+
+        List<Foo> foos = new ArrayList<>();
+
+        // create foos
+        IntStream
+                .range(1, 4)
+                .forEach(i -> foos.add(new Foo("Foo" + i)));
+
+        // create bars
+        foos.forEach(f ->
+                IntStream
+                        .range(1, 4)
+                        .forEach(i -> f.bars.add(new Bar("Bar" + i + " <- " + f.name))));
+
+        // now we have three foos each consisting of three bars
+        foos.stream()
+                .flatMap(f -> f.bars.stream())
+                .forEach(b -> System.out.println(b.name));
+
+        // Bar1 <- Foo1
+        // Bar2 <- Foo1
+        // Bar3 <- Foo1
+        // Bar1 <- Foo2
+        // Bar2 <- Foo2
+        // Bar3 <- Foo2
+        // Bar1 <- Foo3
+        // Bar2 <- Foo3
+        // Bar3 <- Foo3
+
+        // simplification of the above operations
+        IntStream.range(1, 4)
+                .mapToObj(i -> new Foo("Foo" + i))
+                .peek(f -> IntStream.range(1, 4)
+                        .mapToObj(i -> new Bar("Bar" + i + " <- " + f.name))
+                        .forEach(f.bars::add))
+                .flatMap(f -> f.bars.stream())
+                .forEach(b -> System.out.println(b.name));
+
+        // Optional class introduced in java 8
+        // Optionals flatMap operation returns an optional object of another type.
+        // So it can be utilized to prevent nasty null checks.
+
+        // in the past: check to prevent nullPointerException
+        Outer outer = new Outer();
+        if (outer != null && outer.nested != null && outer.nested.inner != null) {
+            System.out.println(outer.nested.inner.foo);
+        }
+
+        // now we can use flatmap
+        // Each call to flatMap returns an Optional wrapping the desired object if present or null if absent.
+        Optional.of(new Outer())
+                .flatMap(o -> Optional.ofNullable(o.nested))
+                .flatMap(n -> Optional.ofNullable(n.inner))
+                .flatMap(i -> Optional.ofNullable(i.foo))
+                .ifPresent(System.out::println);
     }
 
     static class Person {
@@ -311,5 +372,34 @@ public class Java8Stream {
         public String toString() {
             return name;
         }
+    }
+
+    static class Foo {
+        String name;
+        List<Bar> bars = new ArrayList<>();
+
+        Foo(String name) {
+            this.name = name;
+        }
+    }
+
+    static class Bar {
+        String name;
+
+        Bar(String name) {
+            this.name = name;
+        }
+    }
+
+    static class Outer {
+        Nested nested;
+    }
+
+    static class Nested {
+        Inner inner;
+    }
+
+    static class Inner {
+        String foo;
     }
 }
