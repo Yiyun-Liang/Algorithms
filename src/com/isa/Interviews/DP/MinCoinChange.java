bbf680d11e31759624fbbd6cca6fe5a90b2893e4
isa <isaliangyiyun@hotmail.com>
2017-04-16 15:47:24 -0400

    walking in four directions in a 2d array

--- src/com/isa/Interviews/DP/MinCoinChange.java
+++ src/com/isa/Interviews/DP/MinCoinChange.java
@@ -16,21 +16,21 @@ public class MinCoinChange {
         table[0] = 0;
 
         // Initialize all table values as Infinite
-        for (int i = 1; i <= amount; i++)
+        for (int i = 1; i <= amount; i++) {
             table[i] = Integer.MAX_VALUE;
+        }
 
         // Compute minimum coins required for all
         // values from 1 to amount
-        for (int i = 1; i <= amount; i++)
-        {
+        for (int i = 1; i <= amount; i++){
             // Go through all coins smaller than i
-            for (int j = 0; j < typesOfCoins; j++)
-                if (coins[j] <= i)
-                {
-                    int sub_res = table[i-coins[j]];
+            for (int j = 0; j < typesOfCoins; j++) {
+                if (coins[j] <= i) {
+                    int sub_res = table[i - coins[j]];
                     if (sub_res != Integer.MAX_VALUE && sub_res + 1 < table[i])
                         table[i] = sub_res + 1;
                 }
+            }
         }
         return table[amount];
     }
