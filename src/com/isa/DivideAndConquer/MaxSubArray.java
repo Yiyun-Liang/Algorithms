c7417a7c1a603022bfe9eea384909866200a0540
isa <isaliangyiyun@hotmail.com>
2017-04-22 10:45:57 -0400

    add O(n) solution to max subarray probelm

--- src/com/isa/DivideAndConquer/MaxSubArray.java
+++ src/com/isa/DivideAndConquer/MaxSubArray.java
@@ -86,8 +86,43 @@ public class MaxSubArray {
     /*
         O(n) solution to max subarray problem
      */
-    public void maxSubArrayON(){
+    // Approach 1: also kadane’s algorithm ?
+    // does not handle the case where all numbers in the array are negative
 
+    public int max(int[] arr){
+        int maxSum = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            if(sum > maxSum){
+                maxSum = sum;
+            }else if(sum < 0){
+                sum = 0;
+            }
+        }
+
+        return maxSum;
     }
 
+    // Approach 2: Standard Kadane’s Algorithm
+    // find max or min absolute sum of a subarray of an array of integers in O(n)
+    // to find min, change sign of each element in the array and then run Kadane’s Algorithm to find max sum, then invert sign of the max sum found
+    // also handles the case when all numbers in the array are negative
+
+    public int maxSub(int[] arr){
+        int maxEndingHere = arr[0], maxSoFar = arr[0];
+        int[] sum = new int[arr.length];
+        sum[0] = maxSoFar; // memo
+        for(int i = 1; i < arr.length; i++){
+            maxEndingHere = Math.max(maxEndingHere+arr[i], arr[i]);
+            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+            sum[i] = maxSoFar;
+        }
+        return maxSoFar;
+    }
+
+    /*
+        Another similar problem:
+        http://www.geeksforgeeks.org/maximum-absolute-difference-between-sum-of-two-contiguous-sub-arrays/
+     */
 }
